33a9f8dab999b9f6b7e4994fba884a6991b65cd8
andyzzz414 <2297337892@qq.com>
2021-01-01 14:54:56 +0800

    nihaoshijie

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+
+//public表示公开的
+//class表示定义一个类
+//HelloWold表示一个类名
+public class HelloWorld{ //表示定义一个公开的类，起名HelloWoeld
+
+     //类体【记住】
+
+	 /*
+	    public表示公开的
+		static表示静态的
+		void表示空
+		main表示方法名是main
+		(String[] args) 是一个main方法的形式参数列表
+
+		需要记住的是：
+		     以下的方式是一个程序的“主方法”，是程序的执行入口
+			 是sun公司规定的，固定编写方式，
+	*/
+	public static void main(String[] args){ // 表示定义一个公开的静态的主方法
+		//方法体
+		//方法体
+		//方法体
+
+
+		//java语句【Java语句以“;”中止，分号必须是半角分号】
+		//先记住：以下这样代码的作用是向控制台输出一段消息
+		//以下的双引号必须是半角的双引号[是Java语法的一部分】
+		//Java中所有的“字符串”都使用双引号括起来
+		System.out.println("Hello World");
+
+		//再向控制台输出消息
+		System.out.println("Hello sb");
+		
+		//输出中文
+		System.out.println("你好，阿黄");
+		//输出中文【提醒：以下是程序员两边的双引号是全角的】
+        //这里双引号不是Java语法的一部分，这里的全角的双引号只是一个普通的字符串
+		System.out.println("我是一个“程序员”");
+   }
+}
