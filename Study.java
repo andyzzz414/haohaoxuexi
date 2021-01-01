e6aa4940f3b7bcc40054802df4969e2fa9eacea9
andyzzz414 <2297337892@qq.com>
2021-01-01 15:31:02 +0800

    xuexi

---
+++ Study.java
@@ -0,0 +1,10 @@
+public class SonBitch{
+  public static void main(String[] args){
+   System.out.println("好好学习");
+   System.out.println("天天向上");
+   int i = 奥力给;
+	   System.out.println(i);
+       System.out.println(i);
+       System.out.println(i);
+  }
+}
